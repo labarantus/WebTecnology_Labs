0755dca73a550909911956e95f915dc754f9d23b
labarantus <kinologprofe2003d@gmail.com>
2024-05-09 21:44:29 +0500

    Добавил анимации и форму (Антон)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,66 @@
+var h1_hide = 1;
+
+$(document).ready(function() {
+    
+    $(".hidebox p").hide();
+    $(".hidebox h1").css("background-color", "#1e1e1e");
+    $(".hidebox h1").css("color", "#ffffff");
+    $(".page-header span").html()
+
+    $(".product-pic img").hover(function() {
+
+        $(this).animate({
+
+            height: "308",
+            width: "242",
+
+        }, "fast");
+    }, function() {
+
+        $(this).animate({
+
+            height: "280",
+            width: "220",
+
+        }, "fast");
+    });
+
+    $(".hidebox h1").click(function () {
+        if(h1_hide == 1) {
+            h1_hide = 0
+            $(this).next("p").show("slow");
+            $(this).css("background-color", "#FFFFFF");
+            $(this).css("color", "#212529");
+        } else {
+            h1_hide = 1;
+            $(this).next("p").hide("slow");
+            $(this).css("background-color", "#1e1e1e");
+            $(this).css("color", "#ffffff");
+        }
+        
+    });
+
+    $(".product-menu button").hover(function () {
+        $(this).addClass("active"); }).mouseleave(function () {
+        $(this).removeClass("active");
+     });
+
+    $('.product-menu button').click(function() {
+        $(this).parent().parent().find(".selected").removeClass("selected");
+        $(this).addClass("selected");
+       var str = $(this).text().toLowerCase();
+       $(this).parent().parent().parent().parent().find(".page-header1 span").html(str);
+        
+      })
+    
+    $('.form__button').click(function(event) {
+        event.preventDefault();
+    
+        confirm('Отправить отзыв?');
+      })
+
+});
+
 // Забираем таблицу из HTML-файла.
 
 const table = document.querySelector('table');
@@ -53,3 +116,7 @@ table.addEventListener('click', (e) => {
         colIndex = index;
     }
 });
+
+
+
+
