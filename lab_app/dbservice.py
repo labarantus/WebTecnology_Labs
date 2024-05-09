93bddb2aa798ab106e31d5d52e6107fac36a7582
labarantus <kinologprofe2003d@gmail.com>
2024-05-09 21:55:37 +0500

    Добавил добавление данных из формы в БД (Егор)

--- lab_app/dbservice.py
+++ lab_app/dbservice.py
@@ -1,19 +1,23 @@
 from lab_app import db
+from lab_app import Review
+from lab_app import app
 
 def create_contact_req(json_data):
-    try:
-        print(json_data)
-        db.session.execute(f"INSERT INTO review(name, email, reviewtext) VALUES ('аыва', 'аываа', 'ываа')")
+    with app.app_context():
+        try:
+            print(json_data)
+            new_review = Review(name=json_data['client_name'], email=json_data['client_email'], reviewtext=json_data['rev_message'])
+            db.session.add(new_review)
         # Подтверждение изменений в БД
-        print('kdlsada')
-        db.session.commit()
-        print('All Right.')
-        # Возвращаем результат
-        return {'message': "ContactRequest Created!"}
+            print('Added')
+            db.session.commit()
+            print('All Right.')
+            # Возвращаем результат
+            return {'message': "ContactRequest Created!"}
         # если возникла ошибка запроса в БД
-    except Exception as e:
-        # откатываем изменения в БД
-        db.session.rollback()
-        # возвращаем dict с ключом 'error' и текcтом ошибки
-        return {'message': str(e)}
+        except Exception as e:
+            # откатываем изменения в БД
+            db.session.rollback()
+            # возвращаем dict с ключом 'error' и текcтом ошибки
+            return {'message': str(e)}
 
